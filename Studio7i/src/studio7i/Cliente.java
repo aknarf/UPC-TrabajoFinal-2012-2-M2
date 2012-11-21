64b711710c146fbbe2cb3c3b0dcd9184e4d3d083
Frank Agüero <aknarf@gmail.com>
2012-11-21 16:52:08 -0500

    Historia Locales
    Locales, actualizado

---
+++ Studio7i/src/studio7i/Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studio7i;
+
+/**
+ *
+ * @author Frank
+ */
+public class Cliente {
+    String Nombre;
+    String Apellido;
+    
+}
